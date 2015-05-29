816774a938aed568b47cffc5e820300d0cc1f04b
Sorokopud Igor <venglovski@bk.ru>
2015-05-29 05:08:41 +0300

    Revision Content.

--- skin/frontend/iwd/default/js/home_slideshow.js
+++ skin/frontend/iwd/default/js/home_slideshow.js
@@ -1,15 +1,22 @@
 $j(document).ready(function () {
-
-    $j('.home-slideshow-container .home-slideshow')
+    $j('.home-slideshow-container')
         .cycle({
-            slides: '> li',
-            pager: '.home-slideshow-pager',
-            pagerTemplate: '<span class="pager-box"></span>',
             speed: 600,
             pauseOnHover: true,
             swipe: true,
             prev: '.home-slideshow-prev',
             next: '.home-slideshow-next',
+            fx: 'scrollHorz',
+            paused: 'false'
+        });
+
+    $j('.sidebar-left-img-top')
+        .cycle({
+            speed: 600,
+            pauseOnHover: true,
+            swipe: true,
+            prev: '.content-slideshow-prev',
+            next: '.content-slideshow-next',
             fx: 'scrollHorz',
             paused: 'false'
         });
